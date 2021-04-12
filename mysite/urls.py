38590c07709a57ebc72c1e73f75ac895f21edc55
Kavita Maurya <kavita.maurya@capgemini.com>
2021-04-12 19:21:24 +0530

    Html added

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.static import serve
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
+SITE_ROOT = os.path.join(BASE_DIR, 'static')
+print(SITE_ROOT)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': SITE_ROOT, 'show_indexes': True},
+        name='static_path'
+        ),
 ]
